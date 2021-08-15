c1b4d5313d76d167e383e9988394d2c69383d564
marcUsfiLLipe <mf8santos@gmail.com>
2021-08-15 18:14:32 -0300

    Working in table header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
 	<Header @open-form="formOn" />
 	<Form v-if="formStatus" @close-form="formOff" />
+	<Table />
 </template>
 
 <script>
 	import Header from './components/header/Header.vue'
 	import Form from './components/form/Form.vue'
+	import Table from './components/table/Table.vue'
 	export default {
 		name: 'App',
-		components: { Header, Form },
+		components: { Header, Form, Table },
 		data () {
 			return {
 				formStatus: false,
---
+++ src/components/table/Table.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<table>
+			<TableHeader />
+			<!-- <TableBody /> -->
+		</table>
+		<p v-if="!body">
+			Please, insert a stock.
+		</p>
+	</div>
+</template>
+
+<script>
+	import TableHeader from './TableHeader.vue'
+	// import TableBody from './TableBody'
+	import axios from 'axios'
+	export default {
+		name: 'Table',
+		components: { TableHeader },
+		data () {
+			return {
+				body: [],
+			}
+		},
+		created () {
+			axios.get('http://localhost:3000/stock').then(
+				res => {
+					this.body = res.data
+					console.log(this.body)
+				},
+				err => console.log(err)
+			)
+		},
+	}
+</script>
+
+<style scoped>
+	p {
+		color: red;
+		padding-top: 20px;
+		text-align: center;
+	}
+</style>
